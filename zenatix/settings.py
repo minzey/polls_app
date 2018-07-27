6e184995e8f3bad65afd8d28466ddcae1aa49132
Swati Agarwal <minzey@github.com>
2018-07-27 14:29:44 +0530

    allowed hosts to use home url

--- zenatix/settings.py
+++ zenatix/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokuapp.com']
+ALLOWED_HOSTS = ['letspoll.herokuapp.com']
 
 
 # Application definition
